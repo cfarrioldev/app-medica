0cca90e66399ee516f1af07e10d20d711f21285d
cfarrioldev32 <cristianfarriol28@gmail.com>
2025-09-19 21:11:33 +0200

    feat(seeds): implement user seeding with factory and faker

---
+++ mediflow-back/database/seeds/01-user.seed.ts
@@ -0,0 +1,28 @@
+
+import { hash } from 'bcrypt';
+import { User } from '@/users/user.entity';
+import { DataSource } from 'typeorm';
+import { Seeder, SeederFactoryManager } from 'typeorm-extension';
+
+
+export default class UserSeeder implements Seeder {
+  public async run(ds: DataSource, factoryManager: SeederFactoryManager): Promise<void> {
+    const repo = ds.getRepository(User);
+
+    // Admin
+    const adminEmail = 'admin@demo.com';
+    const exists = await repo.findOne({ where: { email: adminEmail } });
+    if (!exists) {
+      await repo.save(repo.create({
+        email: adminEmail,
+        passwordHash: await hash('admin', 10),
+        role: 'ADMIN',
+      }));
+    }
+    const count = await repo.count();
+    if (count < 15) {
+      const userFactory = factoryManager.get(User);
+      await userFactory.saveMany(10);
+    }
+  }
+}
---
+++ mediflow-back/database/seeds/index.ts
@@ -0,0 +1,5 @@
+import UserSeeder from "./01-user.seed";
+
+export const seeders = [
+  UserSeeder,
+];
