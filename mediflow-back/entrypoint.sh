20272496c546d4c03c4b797f0055019e1ef93cef
cfarrioldev32 <cristianfarriol28@gmail.com>
2025-11-17 12:37:09 +0100

    git commit -m "fix: ensure backend API container starts correctly with Docker"

--- mediflow-back/entrypoint.sh
+++ mediflow-back/entrypoint.sh
@@ -1,18 +1,5 @@
-for i in $(seq 1 10); do
-  node -e "
-    const { Client } = require('pg');
-    const c = new Client({
-      host: process.env.DB_HOST,
-      port: +process.env.DB_PORT || 5432,
-      user: process.env.DB_USER,
-      password: process.env.DB_PASS,
-      database: process.env.DB_NAME,
-    });
-    c.connect().then(()=>{console.log('db up'); process.exit(0)}).catch(()=>process.exit(1));
-  " && break
-  echo 'Esperando DB...'; sleep 2
-done
+#!/bin/sh
 
-npx ts-node ./node_modules/typeorm/cli.js -d ./data-source.ts migration:run
+echo "Iniciando API dentro del contenedor..."
 
-node dist/main.js
+node dist/src/main.js
